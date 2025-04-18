05fd923426809e163af52e544ab078e32c935d13
Jkerdley <jk-erley@yandex.ru>
2025-04-18 22:18:50 +0700

    feat: adds typescript typing and volume range

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,90 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 import "./index.scss";
 import { WheatherCard } from "./components/WheatherCard";
+import { VolumeRange } from "./components/VolumeRange";
 
 export function App() {
-    const [activeButton, setActiveButton] = useState<string | null>(null);
+  const [activeButton, setActiveButton] = useState<State>(null);
+  const [volume, setVolume] = useState<number>(0.75);
 
-    const sounds = useRef({
-        first: new Audio("assets/sounds/rain.mp3"),
-        second: new Audio("assets/sounds/winter.mp3"),
-        third: new Audio("assets/sounds/summer.mp3"),
+  const sounds = useRef<IAudio>({
+    rain: new Audio("assets/sounds/rain.mp3"),
+    snow: new Audio("assets/sounds/winter.mp3"),
+    sun: new Audio("assets/sounds/summer.mp3"),
+  });
+
+  const handleButtonClick = (buttonId: State) => {
+    setActiveButton((prev) => (prev === buttonId ? null : buttonId));
+  };
+  const handleVolumeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newVolume = parseFloat(event.target.value);
+    setVolume(newVolume);
+    Object.values(sounds.current).forEach(
+      (sound) => (sound.volume = newVolume)
+    );
+  };
+
+  useEffect(() => {
+    const body = document.body;
+    body.classList.remove("rainy-bg", "winter-bg", "summer-bg");
+
+    Object.values(sounds.current).forEach((sound) => {
+      sound.pause();
+      sound.currentTime = 0;
     });
 
-    const handleButtonClick = (buttonId: string) => {
-        setActiveButton((prev) => (prev === buttonId ? null : buttonId));
-    };
+    if (activeButton === Wheather.Rain) {
+      body.classList.add("rainy-bg");
+      sounds.current.rain.play();
+    } else if (activeButton === Wheather.Snow) {
+      body.classList.add("winter-bg");
+      sounds.current.snow.play();
+    } else if (activeButton === Wheather.Sun) {
+      body.classList.add("summer-bg");
+      sounds.current.sun.play();
+    }
+  }, [activeButton]);
 
-    useEffect(() => {
-        const body = document.body;
-        body.classList.remove("rainy-bg", "winter-bg", "summer-bg");
-
-        Object.values(sounds.current).forEach((sound) => {
-            sound.pause();
-            sound.currentTime = 0;
-        });
-
-        if (activeButton === "first") {
-            body.classList.add("rainy-bg");
-            sounds.current.first.play();
-        } else if (activeButton === "second") {
-            body.classList.add("winter-bg");
-            sounds.current.second.play();
-        } else if (activeButton === "third") {
-            body.classList.add("summer-bg");
-            sounds.current.third.play();
-        }
-    }, [activeButton]);
-
-    return (
-        <section className="main-container">
-            <h1>Звуки природы</h1>
-            <article className="buttons-container">
-                <WheatherCard
-                    src={"assets/icons/pause.svg"}
-                    notPlayingSrc={"assets/icons/cloud-rain.svg"}
-                    buttonClass={"first-button"}
-                    onClick={() => handleButtonClick("first")}
-                    isPlaying={activeButton === "first"}
-                />
-                <WheatherCard
-                    src={"assets/icons/pause.svg"}
-                    notPlayingSrc={"assets/icons/cloud-snow.svg"}
-                    buttonClass={"second-button"}
-                    onClick={() => handleButtonClick("second")}
-                    isPlaying={activeButton === "second"}
-                />
-                <WheatherCard
-                    src={"assets/icons/pause.svg"}
-                    notPlayingSrc={"assets/icons/sun.svg"}
-                    buttonClass={"third-button"}
-                    onClick={() => handleButtonClick("third")}
-                    isPlaying={activeButton === "third"}
-                />
-            </article>
-        </section>
-    );
+  return (
+    <section className="main-container">
+      <h1>Звуки природы</h1>
+      <article className="buttons-container">
+        <WheatherCard
+          src={"assets/icons/pause.svg"}
+          notPlayingSrc={"assets/icons/cloud-rain.svg"}
+          buttonClass={"rain-button"}
+          onClick={() => handleButtonClick(Wheather.Rain)}
+          isPlaying={activeButton === Wheather.Rain}
+        />
+        <WheatherCard
+          src={"assets/icons/pause.svg"}
+          notPlayingSrc={"assets/icons/cloud-snow.svg"}
+          buttonClass={"snow-button"}
+          onClick={() => handleButtonClick(Wheather.Snow)}
+          isPlaying={activeButton === Wheather.Snow}
+        />
+        <WheatherCard
+          src={"assets/icons/pause.svg"}
+          notPlayingSrc={"assets/icons/sun.svg"}
+          buttonClass={"sun-button"}
+          onClick={() => handleButtonClick(Wheather.Sun)}
+          isPlaying={activeButton === Wheather.Sun}
+        />
+      </article>
+      <VolumeRange volume={volume} handleVolumeChange={handleVolumeChange} />
+    </section>
+  );
 }
+
+enum Wheather {
+  Rain = "rain",
+  Snow = "snow",
+  Sun = "sun",
+}
+interface IAudio {
+  rain: HTMLAudioElement;
+  snow: HTMLAudioElement;
+  sun: HTMLAudioElement;
+}
+
+type State = Wheather.Rain | Wheather.Snow | Wheather.Sun | null;
--- src/components/WheatherCard.tsx
+++ src/components/WheatherCard.tsx
@@ -1,27 +1,28 @@
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 import "../index.scss";
-interface WheatherCardProps {
-    src: string;
-    notPlayingSrc: string;
-    buttonClass: string;
-    onClick: () => void;
-    isPlaying: boolean;
-}
 
 export const WheatherCard: FC<WheatherCardProps> = ({
-    src,
-    notPlayingSrc,
-    buttonClass,
-    onClick,
-    isPlaying,
+  src,
+  notPlayingSrc,
+  buttonClass,
+  onClick,
+  isPlaying,
 }) => {
-    return (
-        <button className={buttonClass} onClick={onClick}>
-            {isPlaying ? (
-                <img src={src} alt="playing wheather"></img>
-            ) : (
-                <img src={notPlayingSrc} alt="paused"></img>
-            )}
-        </button>
-    );
+  return (
+    <button className={buttonClass} onClick={onClick}>
+      {isPlaying ? (
+        <img src={src} alt="playing wheather"></img>
+      ) : (
+        <img src={notPlayingSrc} alt="paused"></img>
+      )}
+    </button>
+  );
 };
+
+interface WheatherCardProps {
+  src: string;
+  notPlayingSrc: string;
+  buttonClass: string;
+  onClick: () => void;
+  isPlaying: boolean;
+}
